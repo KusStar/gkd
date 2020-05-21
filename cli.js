e26da036d808069ff05ba41f565105dae916b7a4
Kuss <2360314753@qq.com>
2020-05-21 10:15:25 +0800

    :art: Format: cli.js

--- cli.js
+++ cli.js
@@ -14,25 +14,25 @@ const cli = meow(`
 
   Examples
       $ gkd --help
-      $ gkd app-name -t react-ts-webpack-template
-      $ gkd app-name -t react-ts-webpack-template
+      $ gkd app-name -t react-ts-webpack
+      $ gkd app-name -t react-ts-webpack
   `,
-{
-flags: {
-  template: {
-    type: 'string',
-    alias: 't'
-  },
-  ['author.name']: {
-    type: 'string',
-    default: 'KusStar',
-  },
-  ['author.page']: {
-    type: 'string',
-    default: 'https://github.com/KusStar',
-  },
-}
-}
+  {
+    flags: {
+      template: {
+        type: 'string',
+        alias: 't'
+      },
+      ['author.name']: {
+        type: 'string',
+        default: 'KusStar',
+      },
+      ['author.page']: {
+        type: 'string',
+        default: 'https://github.com/KusStar',
+      },
+    }
+  }
 );
 
 gkd(cli.input, cli.flags);
