80997ddc2c831e188211d494805ec4b3f27d63b0
Kuss <2360314753@qq.com>
2020-05-21 20:45:47 +0800

    :recycle: Refactor gkd.js and create utils.js

---
+++ src/utils.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const fs = require('fs-extra')
+const path = require('path')
+const chalk = require('chalk')
+const { isYarn } = require('is-npm')
+
+const _logPackageScripts = (appName) => {
+  try {
+    const target = path.join(process.cwd(), appName, 'package.json')
+    const raw = fs.readFileSync(target, { encoding: 'utf8' })
+    const json = JSON.parse(raw)
+    const scripts = Object.keys(json.scripts)
+    // log
+    console.log(chalk.bold.underline.greenBright(`Scripts:`))
+    console.log()
+    for (const script of scripts) {
+      console.log(`> ${isYarn ? 'yarn' : 'npm'} run ${chalk.bold(script)}`)
+    }
+  } catch (_) {
+    // just catch
+  }
+}
+
+const logSuccess = (appName) => {
+  console.log()
+  console.log(chalk.bold.underline.greenBright(`Start:`))
+  console.log()
+  console.log(`> cd ${appName}`)
+  console.log(`> ls -al`)
+  console.log()
+  _logPackageScripts(appName)
+  console.log()
+}
+
+const logRequireArgs = (...args) => {
+  for (const arg of args) {
+    console.log()
+    console.log(`${chalk.red('X')} ${chalk.cyan(arg)} is required`)
+    console.log()
+  }
+}
+
+module.exports = {
+  logSuccess,
+  logRequireArgs,
+}
