9f732c16faa44d6c8e6518b6deb5c265a2eb8152
Kuss <2360314753@qq.com>
2021-06-06 22:32:22 +0800

    :bento: assets: new template -> react-ts-vite

---
+++ templates/react-ts-vite/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './App.css'
+
+interface Props {}
+
+const App: FC<Props> = () => {
+  return (
+    <h1>Hello, %NAME%</h1>
+  )
+}
+
+export default App;
---
+++ templates/react-ts-vite/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: [{
+      find: '@',
+      replacement: path.resolve(__dirname, 'src')
+    }]
+  }
+})
